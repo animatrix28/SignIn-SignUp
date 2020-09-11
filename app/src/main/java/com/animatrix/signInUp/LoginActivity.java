ab59cb1be80c454de645afa92ac200b74554c3f2
Animesh Srivastava <as280899@gmail.com>
2020-09-11 16:42:28 +0530

    Login/Register

---
+++ app/src/main/java/com/animatrix/signInUp/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.animatrix.signInUp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    // TODO: Add member variables here:
+    private FirebaseAuth mAuth;
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailView = findViewById(R.id.login_email);
+        mPasswordView = findViewById(R.id.login_password);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.integer.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // TODO: Grab an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    // Executed when Sign in button pressed
+    public void signInExistingUser(View v)   {
+        // TODO: Call attemptLogin() here
+        attemptLogin();
+
+    }
+
+    // Executed when Register button pressed
+    public void registerNewUser(View v) {
+        Intent intent = new Intent(this, com.animatrix.signInUp.RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    // TODO: Complete the attemptLogin() method
+    private void attemptLogin() {
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if (email.isEmpty())
+            if (email.equals("") || password.equals("")) return;
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
+        // TODO: Use FirebaseAuth to sign in with email & password
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                Log.d("FlashChat", "signInWithEmail() onComplete: " + task.isSuccessful());
+
+                if (!task.isSuccessful()) {
+                    Log.d("FlashChat", "Problem signing in: " + task.getException());
+                    showErrorDialog("There was a problem signing in");
+                } else {
+                    showSuccessDialog("Welcome, Your Login Was Successful");
+                }
+
+            }
+        });
+
+
+    }
+    private void showErrorDialog(String message) {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+    private void showSuccessDialog(String message) {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Login Successful")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.star_big_on)
+                .show();
+    }
+
+}
